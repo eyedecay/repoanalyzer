62c05db35828354e37c6c6f99c7aa38b73424152
johnathan <notbuffalo09@gmail.com>
2026-07-16 18:37:18 -0400

    setup daisyUI

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,9 +25,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-theme = "caramellate"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-base-100 text-base-content">{children}</body>
     </html>
   );
 }

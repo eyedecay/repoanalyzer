bc7d5b2f165091387ea709c8b4a54c5fd14bd258
johnathan <notbuffalo09@gmail.com>
2026-07-16 21:55:15 -0400

    title

--- frontend/app/components/InputCard.tsx
+++ frontend/app/components/InputCard.tsx
@@ -7,10 +7,13 @@ const InputCard = () => {
     const [repoUrl, setRepoUrl] = useState("")
     return (
         <>
+        <div>
+            <h1 className = "text-4xl font-bold text-center mb-5"> RepoAnalyzer</h1>
             <div className = "join w-full max-w-sm shadow-2xl">
                 <InputBox value = {repoUrl} onChange = {setRepoUrl}/>
                 <AnalyzeButton repoUrl = {repoUrl}/>
             </div>
+        </div>
         </>
     )
 }

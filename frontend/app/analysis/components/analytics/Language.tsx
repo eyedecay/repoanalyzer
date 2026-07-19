345dde6ee0a3235b921329bf17383bcf51cc3798
johnathan <notbuffalo09@gmail.com>
2026-07-19 14:05:58 -0400

    feat: language logo

--- frontend/app/analysis/components/analytics/Language.tsx
+++ frontend/app/analysis/components/analytics/Language.tsx
@@ -3,12 +3,15 @@ interface LanguageProp {
 }
 
 const Language = ({language}: LanguageProp) => {
+    const iconName = language.toLowerCase().replace("#", "sharp").replace("++", "plusplus".replace(" ", ""))
+
+    const imageUrl = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${iconName}/${iconName}-original.svg`;
     return (
         <div className="card bg-base-100 w-48 shadow-sm">
         <div className="hover-3d">
             {/* content */}
             <figure className="h-28 max-w-100 rounded-2xl">
-                <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
+                <img src= {imageUrl} alt="3D card" className = "w-28 h-28 object-contain" />
             </figure>
             {/* 8 empty divs needed for the 3D effect */}
             <div></div>
@@ -21,7 +24,7 @@ const Language = ({language}: LanguageProp) => {
             <div></div>
         </div>
         <div className="card-body">
-            <h2 className="card-title justify-center" >Stars: {language} </h2>
+            <h2 className="card-title justify-center" >{language} </h2>
 
         </div>
         </div>

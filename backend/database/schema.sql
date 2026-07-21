8cd04b3ee7b33dcd0449f2ffa4c73efc52b4c9ea
johnathan <notbuffalo09@gmail.com>
2026-07-20 20:49:16 -0400

    unique sql thing

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,10 +1,12 @@
 CREATE table chunk_vectors(
     id SERIAL PRIMARY KEY, 
-    owner TEXT NOT NULL,
-    repo TEXT NOT NULL,
+    repo_owner TEXT NOT NULL,
+    repo_name TEXT NOT NULL,
     file_path TEXT NOT NULL,
     extension TEXT, 
-    chunk_index INTEGER NOT NULL
-    CONTENT TEXT 
+    chunk_index INTEGER NOT NULL,
+    CONTENT TEXT,
     embedding vector(768)
-)
+
+    UNIQUE(repo_owner, repo_name, file_path, chunk_index)
+);
